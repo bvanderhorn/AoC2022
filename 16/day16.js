4e6990887239dea575f0b07a35aa6a0707ba9cda
bvanderhorn <bvanderhorn@gmail.com>
2022-12-22 23:18:03 +0100

    feat: working test set

--- 16/day16.js
+++ 16/day16.js
@@ -11,11 +11,14 @@ function writeFile(filename, content) {
 function getValve(valve) {
     return valves.filter(v => v.name === valve)[0];
 }
-function unvisited(valves, visited) {
-    return valves.filter(v => !visited.includes(v));
-}
-function isOpen(valve) {
-    return flow.map(f => f.name).includes(valve);
+// function unvisited(valves:string[],visited:string[]) : string[] {
+//     return valves.filter(v=> !visited.includes(v));
+// }
+// function isOpen(valve:string) : boolean {
+//     return flow.map(f=>f.name).includes(valve);
+// }
+function getValveIndex(valve) {
+    return valves.map((v, index) => v.name == valve ? index : -1).filter(i => i >= 0)[0];
 }
 function shortestPathMap(valve) {
     // returns a list of shortest paths from given valve to valves on matching 'valves' indices
@@ -58,14 +61,39 @@ function shortestPathMap(valve) {
     // add each distance name to path and update potential
     dist.forEach(d => {
         d.path.push(d.name);
-        d.potential = (minutes - d.distance) * d.rate;
     });
-    return dist.sort((a, b) => a.potential > b.potential ? -1 : 1);
+    return dist;
 }
-function highestPotential(valve, minute) {
-    var distances = getValve(valve).pathmap.filter(d => d.rate > 0 && !isOpen(d.name));
-    distances.forEach(d => d.potential = (minutes - minute - d.distance) * d.rate);
-    return distances.sort((a, b) => a.potential > b.potential ? -1 : 1)[0];
+function getMaxWithRemaining(curValve, rem, remMin, prevPath) {
+    // recursive function trying each remaining valve as next to see which one will return the highest total flow
+    var scores = [];
+    var cur = getValve(curValve);
+    for (let i = 0; i < rem.length; i++) {
+        let vi = getValve(rem[i]);
+        let dist = cur.pathmap.filter(p => p.name === vi.name)[0].distance;
+        if (remMin - dist - 1 <= 0)
+            continue;
+        else {
+            let sc = getMaxWithRemaining(rem[i], rem.filter(v => v != vi.name), remMin - dist - 1, [...prevPath, rem[i]]);
+            let fl = (remMin - dist - 1) * vi.rate;
+            let path = [];
+            if (sc.length > 0) {
+                fl += sc[0].flow;
+                path = sc[0].path;
+            }
+            path.unshift(vi.name);
+            scores.push({
+                flow: fl,
+                path: path
+            });
+        }
+        if (prevPath.length === 1) {
+            var totalCount = (valves.length - 1) * (valves.length - 2);
+            var curCount = (getValveIndex(prevPath[0]) - 1) * (valves.length - 2) + i + 1;
+            console.log(" " + (curCount / totalCount * 100).toPrecision(2) + "% done");
+        }
+    }
+    return scores.sort((a, b) => a.flow > b.flow ? -1 : 1);
 }
 // params
 const inputFile = 'example_valves.txt';
@@ -73,7 +101,7 @@ const start = "AA";
 const minutes = 30;
 // parse
 const input = fs.readFileSync(inputFile, 'utf8');
-const valves = input.split('\r\n').map(v => {
+var valves = input.split('\r\n').map(v => {
     var vMatch = v.match(/Valve\s+(\w+)\s+has flow rate=(\d+); tunnels? leads? to valves?\s+([\s\S]+)$/);
     return {
         name: vMatch[1],
@@ -82,54 +110,15 @@ const valves = input.split('\r\n').map(v => {
         pathmap: []
     };
 }).sort((a, b) => a.name < b.name ? -1 : 1);
-// add shortest path maps
-valves.forEach(v => v.pathmap = shortestPathMap(v.name));
+// add shortest path maps (strip pathmaps of valves which are not the start valve and have rate 0)
+valves.forEach(v => v.pathmap = shortestPathMap(v.name).filter(p => p.name === start || p.rate > 0));
+// only keep valves which are start or have rate > 0
+valves = valves.filter(v => v.name === start || v.rate > 0);
+// save some stuff to look at
 writeFile('valves_sorted.json', stringify(valves));
-writeFile("valve_AA.json", stringify(valves[0]));
 var longestDistance = valves.map(v => v.pathmap.sort((a, b) => a.distance > b.distance ? -1 : 1)[0]).sort((a, b) => a.distance > b.distance ? -1 : 1)[0];
-// console.log(" longest distance: " + stringify(longestDistance));
-var visited = [start];
-var flow = [];
-var cur = getValve(start);
-for (let m = 1; m < minutes; m++) {
-    // Instead:
-    // 1. for the current valve and the current minute, find out the path to which yet-unopened valve has the highest flow potential
-    // 2. If current valve is unopened and rate > 0:
-    //     a. calculate the flow potential of opening the current valve
-    //     b. calculate the path with the highest flow potential if the path would continue another minute from now
-    //     c. add (a) and (b) 
-    //     d. if higher than (1.): open valve and continue
-    //        else: move to next valve on path from (1.) and continue
-    if (flow.length == valves.filter(v => v.rate > 0).length) {
-        console.log(" --> all valves have been opened -> end");
-        break;
-    }
-    var comment = "minute " + m + ", at " + cur.name + " (r " + cur.rate + "): ";
-    var hp = highestPotential(cur.name, m);
-    var hpPot = hp.potential;
-    // calculate current potential, add future path potential on m+1
-    var hpplus = highestPotential(cur.name, m + 1);
-    var hppPot = hpplus.potential;
-    var cp = (minutes - m) * cur.rate;
-    var openBasedOnPotential = (cp + hppPot) > hpPot;
-    if (cur.rate > 0 && (!isOpen(cur.name)) && openBasedOnPotential) {
-        let fl = (minutes - m) * cur.rate;
-        flow.push({
-            name: cur.name,
-            minute: m,
-            rate: cur.rate,
-            flow: fl
-        });
-        console.log(comment + "open and count flow " + (minutes - m) + "*" + cur.rate + " = " + fl);
-    }
-    else {
-        // if rate is 0 or valve already opened or not worth opening valve: move to next valve on the highest-potential track
-        cur = getValve(hp.path[1]);
-        if (!visited.includes(cur.name))
-            visited.push(cur.name);
-        console.log(comment + "move to " + cur.name + " (towards " + hp.name + ": dist. " + hp.distance + ", potential " + hpPot + ")");
-    }
-}
-console.log("total flow: " + flow.map(f => f.flow).reduce((a, b) => a + b, 0));
-writeFile("flow.json", stringify(flow));
+console.log(" longest distance: " + stringify(longestDistance));
+// permutator
+var maxScore = getMaxWithRemaining(start, valves.map(v => v.name).filter(v => v != start), minutes, [])[0];
+console.log(stringify(maxScore));
 //# sourceMappingURL=day16.js.map
--- 16/day16.ts
+++ 16/day16.ts
@@ -16,9 +16,13 @@ type Distance = {
     name:string,
     distance:number,
     rate: number,
-    potential:number,
     to: string[],
     path:string[]
+}
+
+type Score = {
+    flow: number,
+    path: string[]
 }
 function stringify(object: any) : string {
     return JSON.stringify(object, null, 4);
@@ -30,11 +34,14 @@ function writeFile(filename:string,content:string) {
 function getValve(valve:string) : Valve {
     return valves.filter(v=>v.name===valve)[0];
 }
-function unvisited(valves:string[],visited:string[]) : string[] {
-    return valves.filter(v=> !visited.includes(v));
-}
-function isOpen(valve:string) : boolean {
-    return flow.map(f=>f.name).includes(valve);
+// function unvisited(valves:string[],visited:string[]) : string[] {
+//     return valves.filter(v=> !visited.includes(v));
+// }
+// function isOpen(valve:string) : boolean {
+//     return flow.map(f=>f.name).includes(valve);
+// }
+function getValveIndex(valve:string) : number {
+    return valves.map((v,index)=> v.name==valve ? index : -1).filter(i=>i>=0)[0];
 }
 function shortestPathMap(valve:string): Distance[] {
     // returns a list of shortest paths from given valve to valves on matching 'valves' indices
@@ -78,14 +85,39 @@ function shortestPathMap(valve:string): Distance[] {
     // add each distance name to path and update potential
     dist.forEach(d=>{
         d.path.push(d.name);
-        d.potential = (minutes-d.distance)*d.rate;
     });
-    return dist.sort((a,b)=> a.potential > b.potential ? -1 : 1);
+    return dist;
 }
-function highestPotential(valve:string,minute:number) : Distance {
-    var distances = getValve(valve).pathmap.filter(d=>d.rate > 0 && !isOpen(d.name));
-    distances.forEach(d=>d.potential = (minutes-minute-d.distance)*d.rate);
-    return distances.sort((a,b) => a.potential > b.potential ? -1 : 1)[0];
+
+function getMaxWithRemaining(curValve:string,rem:string[],remMin:number,prevPath: string[]) : Score[] {
+    // recursive function trying each remaining valve as next to see which one will return the highest total flow
+    var scores : Score[] = [];
+    var cur = getValve(curValve);
+    for (let i=0;i<rem.length;i++) {
+        let vi = getValve(rem[i]);
+        let dist = cur.pathmap.filter(p=>p.name===vi.name)[0].distance;
+        if (remMin-dist-1 <= 0) continue;
+        else {
+            let sc = getMaxWithRemaining(rem[i], rem.filter(v=>v!=vi.name), remMin - dist-1,[...prevPath,rem[i]]);
+            let fl = (remMin - dist-1)*vi.rate;
+            let path = [];
+            if (sc.length >0) {
+                fl += sc[0].flow;
+                path = sc[0].path;
+            }
+            path.unshift(vi.name);
+            scores.push({
+                flow: fl,
+                path: path
+            });
+        }
+        if (prevPath.length===1) {
+            var totalCount = (valves.length-1)*(valves.length-2);
+            var curCount = (getValveIndex(prevPath[0])-1)*(valves.length-2) + i + 1;
+            console.log(" " + (curCount/totalCount*100).toPrecision(2) + "% done");
+        }
+    }
+    return scores.sort((a,b)=> a.flow > b.flow ? -1 : 1);
 }
 
 // params
@@ -95,7 +127,7 @@ const minutes = 30;
 
 // parse
 const input: string = fs.readFileSync(inputFile, 'utf8');
-const valves: Valve[] = input.split('\r\n').map(v => {
+var valves: Valve[] = input.split('\r\n').map(v => {
     var vMatch = v.match(/Valve\s+(\w+)\s+has flow rate=(\d+); tunnels? leads? to valves?\s+([\s\S]+)$/);
     return {
         name: vMatch[1],
@@ -105,55 +137,16 @@ const valves: Valve[] = input.split('\r\n').map(v => {
     }
 }).sort((a,b) => a.name < b.name ? -1 : 1);
 
-// add shortest path maps
-valves.forEach(v=> v.pathmap = shortestPathMap(v.name));
+// add shortest path maps (strip pathmaps of valves which are not the start valve and have rate 0)
+valves.forEach(v=> v.pathmap = shortestPathMap(v.name).filter(p=>p.name===start || p.rate>0));
+// only keep valves which are start or have rate > 0
+valves = valves.filter(v=>v.name===start || v.rate>0);
+
+// save some stuff to look at
 writeFile('valves_sorted.json',stringify(valves));
-writeFile("valve_AA.json",stringify(valves[0]));
 var longestDistance = valves.map(v=> v.pathmap.sort((a,b)=>a.distance > b.distance ? -1 : 1)[0]).sort((a,b)=>a.distance > b.distance ? -1 : 1)[0];
-// console.log(" longest distance: " + stringify(longestDistance));
+console.log(" longest distance: " + stringify(longestDistance));
 
-var visited: string[] = [start];
-var flow: Flow[] = [];
-var cur = getValve(start);
-for (let m=1;m<minutes;m++) {
-    // Instead:
-    // 1. for the current valve and the current minute, find out the path to which yet-unopened valve has the highest flow potential
-    // 2. If current valve is unopened and rate > 0:
-    //     a. calculate the flow potential of opening the current valve
-    //     b. calculate the path with the highest flow potential if the path would continue another minute from now
-    //     c. add (a) and (b) 
-    //     d. if higher than (1.): open valve and continue
-    //        else: move to next valve on path from (1.) and continue
-
-    if (flow.length == valves.filter(v=>v.rate >0).length) {
-        console.log(" --> all valves have been opened -> end");
-        break;
-    }
-
-    var comment = "minute " + m + ", at " + cur.name + " (r " + cur.rate + "): ";
-    var hp = highestPotential(cur.name, m);
-    var hpPot = hp.potential;
-    // calculate current potential, add future path potential on m+1
-    var hpplus = highestPotential(cur.name, m+1);
-    var hppPot = hpplus.potential;
-    var cp = (minutes-m)*cur.rate;
-    var openBasedOnPotential : boolean = (cp+hppPot) > hpPot;
-    if (cur.rate > 0 && (!isOpen(cur.name)) && openBasedOnPotential) {
-        let fl = (minutes-m)*cur.rate;
-        flow.push({
-            name:cur.name,
-            minute:m,
-            rate: cur.rate,
-            flow: fl
-        });
-        console.log(comment +"open and count flow " + (minutes-m) + "*"+cur.rate+" = " + fl );
-    } else  {
-        // if rate is 0 or valve already opened or not worth opening valve: move to next valve on the highest-potential track
-        cur = getValve(hp.path[1]);
-        if (!visited.includes(cur.name)) visited.push(cur.name);
-        console.log(comment +"move to " + cur.name + " (towards " + hp.name + ": dist. " + hp.distance + ", potential " + hpPot + ")");
-    }
-}
-console.log("total flow: " + flow.map(f=>f.flow).reduce((a,b)=> a+b,0));
-writeFile("flow.json",stringify(flow));
-
+// permutator
+var maxScore = getMaxWithRemaining(start, valves.map(v=>v.name).filter(v=>v!=start),minutes,[])[0];
+console.log(stringify(maxScore));
