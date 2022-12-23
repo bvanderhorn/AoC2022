4c1eb06f2e41c844f9cf8bba58e3440a2e06dba6
bvanderhorn <bvanderhorn@gmail.com>
2022-12-23 22:39:34 +0100

    feat: day19 parse

---
+++ 19/day19.py
@@ -0,0 +1,23 @@
+import numpy as np
+import re
+
+# functions
+def readFile(fName):
+    inStream = open(fName,'r')
+    out = inStream.read()
+    inStream.close()
+    return out
+
+def writeFile(fName,outString):
+    outStream = open(fName,'w')
+    outStream.write(outString)
+    outStream.close()
+    
+# parse
+input = readFile('blueprints.txt')
+blueprintStrings = input.split('\n')
+blueprints = []
+for bs in blueprintStrings:
+    m = re.match("^\D+(\d+)\D+(\d+)\D+(\d+)\D+(\d+)\D+(\d+)\D+(\d+)\D+(\d+)\D+$",bs)
+    blueprints.append([int(i) for i in m.group(1,2,3,4,5,6,7)])
+print(blueprints)
