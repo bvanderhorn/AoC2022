289666825c05e1b20e5d214bfbe4dc42059197ba
bvanderhorn <bvanderhorn@gmail.com>
2022-12-25 10:28:14 +0100

    day19 another update

--- 19/day19.py
+++ 19/day19.py
@@ -28,28 +28,40 @@ def getRobot(blueprint, robots, assets, remaining, robot):
     newRobots[robot] += 1
     return [newRobots, newAssets, newRemaining]
             
-def getMaxWithRemaining(blueprint, robots, assets, remaining, depth):
+def getMaxWithRemaining(blueprint, robots, assets, remaining, depth, path ):
     # blueprint, robots and assets are 4x1 arrays in format [ore, clay, obsidian, geode]
     global maxAssets
-    
+    global earliestFirstGeodeRobotAtMinute
+    mins = minutes-remaining+1
     scores = []
+     # if it's been more than the current global earliest minute at which you could have finished a first geode 
+    # robot, and you haven't: just forget about this branch
+    if (mins > earliestFirstGeodeRobotAtMinute+ 1) & (assets[3] == 0):
+        return scores
+    
     # if you have robots ore and clay (1,2), you can build robots ore, clay and obs (1,2,3)
     emptyRobots = [i for i, x in enumerate(robots) if x==0]
     if len(emptyRobots) > 0:  firstEmptyRobot = emptyRobots[0]
     else: firstEmptyRobot = 3
     # print(" "*d + " min "+ str(minutes-remaining+1)+ ","+ " up to robot: "+ str(firstEmptyRobot+1))
         
-    for r in range(0, firstEmptyRobot+1):
+    for r in range(firstEmptyRobot, -1,-1):
         # print(" "*(d+1) + " build robot " + str(r) )
         next = getRobot(blueprint, robots, assets, remaining, r)
         if next[2] > 0 :
-            sc = getMaxWithRemaining(blueprint, next[0],next[1],next[2], depth + 1)
+            sc = getMaxWithRemaining(blueprint, next[0],next[1],next[2], depth + 1, [i for i in path] + [[r, next[2]]])
             scores.append(sc[0])
+            if (r == len(blueprint)) & ((minutes-next[2]+1) < earliestFirstGeodeRobotAtMinute):
+                    # if you have the assets to make a geode robot:
+                    # save this minute if it is smaller than the previous earliest minute
+                    print(" could create first geode robot already at minute "+str((minutes-next[2]+1)) + "!")
+                    earliestFirstGeodeRobotAtMinute = (minutes-next[2]+1)
     if len(scores) == 0:
         newAssets = list(np.add(assets, np.multiply(robots, remaining)))
         if newAssets[3] > maxAssets[3]:
             maxAssets = [i for i in newAssets]
             print(" new max assets: " + str(maxAssets))
+            print("  path: "+ str(path))
         scores.append(assets[3] + robots[3]*remaining)
     return sorted(scores, reverse = True)
     
@@ -83,12 +95,12 @@ mainRobots = [1,0,0,0]
 mainAssets = [0,0,0,0]
 
 for bpi in range(0, 1):
-    print("blueprint: " + str(bpi+1))
+    print("blueprint " + str(bpi+1) + ": " + str(blueprints[bpi]))
     bplog = []
     bp = blueprints[bpi]
     maxAssets = [0,0,0,0]
     earliestFirstGeodeRobotAtMinute = minutes+1
-    sc = getMaxWithRemaining(bp, mainRobots, mainAssets, minutes, 0)
+    sc = getMaxWithRemaining(bp, mainRobots, mainAssets, minutes, 0, [])
     mainScores.append(sc[0])
     
 qualityLevels = list(np.multiply(mainScores,[i+1 for i in range(0, len(mainScores))]))
