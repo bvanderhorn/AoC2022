69a2203d03c94377f6d58c8e9eca66f690ee7f67
bvanderhorn <bvanderhorn@gmail.com>
2022-12-26 11:24:30 +0100

    feat: part 2 some progress

--- 21/day21.py
+++ 21/day21.py
@@ -1,5 +1,4 @@
 import numpy as np
-import json
 import re
 
 # functions
@@ -9,22 +8,49 @@ def readFile(fName):
     inStream.close()
     return out
 
-def evaluate(monkeyName):
-    monkey = [m for m in monkeys if m[0] == monkeyName][0]
+def writeFile(fName,outString):
+    outStream = open(fName,'w')
+    outStream.write(outString)
+    outStream.close()
+
+def print2(inString):
+    if test: print(inString)
+
+def getMonkey(monkeyName):
+    return [m for m in monkeys if m[0] == monkeyName][0]
+
+def getMonkeyIndex(monkeyName):
+    return [i for (i,m) in enumerate(monkeys) if m[0] == monkeyName][0]
+
+def evaluate(monkeyName, extend, depth):
+    monkey = getMonkey(monkeyName)
+    print2(" "*depth + " evaluate " + monkeyName + ": " + monkey[1])
     name = monkey[0]
     equate = monkey[1]
     equationMatch = re.match("(\w+) \W (\w+)",equate)
     intMatch = re.match("^\d+$",equate)
-    if (intMatch != None):
+    out = ""
+    if (monkeyName == human) & extend: 
+        print2(" "*depth + "  --> return " + human)
+        return human
+    elif (intMatch != None):
         return equate
     else:
         m1 = equationMatch.group(1)
         m2 = equationMatch.group(2)
-        equate = equate.replace(m1,evaluate(m1)).replace(m2,evaluate(m2))
-    return str(eval(equate))
+        equate = equate.replace(m1,evaluate(m1,extend,depth+1)).replace(m2,evaluate(m2,extend,depth+1))
+        
+    print2(" "*depth + " returning " + monkeyName + ": " + equate)
+    if extend:
+        return "("+ equate +")"
+    else:
+        return(str(eval(out)))
 
 # params
 fileName = 'monkeys.txt'
+human = 'humn'
+root = 'root'
+test = False
 
 # parse
 input = readFile(fileName).split('\n')
@@ -33,4 +59,16 @@ for i in range(0,len(input)):
     monkeyMatch = re.match("^(\w+): ([\s\S]+)$",input[i])
     monkeys.append(list(monkeyMatch.group(1,2)))
 
-print(evaluate('root'))
+# part 1
+# print(evaluate(root))
+
+# part 2
+# update root equation
+rootMonkey = getMonkey(root)
+rootOriginalOperation = re.match("\w+ (\W) \w+",rootMonkey[1]).group(1)
+monkeys[getMonkeyIndex(root)][1] = rootMonkey[1].replace(rootOriginalOperation, "=")
+print(monkeys[getMonkeyIndex(root)])
+
+rootEvaluation = evaluate(root,True,0)
+print(rootEvaluation)
+writeFile('rootEval.txt',rootEvaluation)
