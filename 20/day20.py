b30ae45928a5cf680d409c41e07e221c0241057d
bvanderhorn <bvanderhorn@gmail.com>
2022-12-26 09:20:52 +0100

    feat: day20 part2

--- 20/day20.py
+++ 20/day20.py
@@ -12,18 +12,24 @@ def readFile(fName):
 fileName = 'file.txt'
 key = 0
 after = [1000,2000,3000]
+encryptionKey = 811589153
+rounds = 10
 
 # parse
 input = readFile(fileName)
-bareNumbers = [int(i) for i in input.split('\n')]
+bareNumbers = [int(i)*encryptionKey for i in input.split('\n')]
 numbers = [{'value':j,'index':idx} for (idx,j) in enumerate(bareNumbers)]
-print(json.dumps(numbers[0:3],indent=4))
+# print(json.dumps(numbers[0:3],indent=4))
+
+# run
 totalAmount = len(bareNumbers)
-for index in range(0,totalAmount):
-    curIndex = [i for (i,n) in enumerate(numbers) if n['index']==index][0]
-    number = numbers.pop(curIndex)
-    newIndex = (number['value'] + curIndex) % (totalAmount-1)
-    numbers.insert(newIndex, number)
+for j in range(0,rounds):
+    print(' round '+ str(j+1) + '...')
+    for index in range(0,totalAmount):
+        curIndex = [i for (i,n) in enumerate(numbers) if n['index']==index][0]
+        number = numbers.pop(curIndex)
+        newIndex = (number['value'] + curIndex) % (totalAmount-1)
+        numbers.insert(newIndex, number)
 
 zeroIndex = [i for (i,n) in enumerate(numbers) if n['value']==0][0]
 getIndices = np.mod(np.add(after,zeroIndex), totalAmount)
