87ba69f4c5cea45aa74d9c57555fb92a59319bbe
bvanderhorn <bvanderhorn@gmail.com>
2022-12-26 08:17:09 +0100

    feat: day20 part1

--- 20/day20.py
+++ 20/day20.py
@@ -9,8 +9,13 @@ def readFile(fName):
     return out
 
 def getNextNotMoved(array1):
-    notMoved = [i for i,a in array1 if a['moved']==False]
+    notMoved = [i for (i,a) in enumerate(array1) if a['moved']==False]
     return (-1, notMoved[0])[len(notMoved)>0]
+
+def getNewIndex(index, move, arrayLength):
+    others = arrayLength -1
+    return (move + index) % others
+    
 
 # params
 fileName = 'file.txt'
@@ -20,11 +25,25 @@ after = [1000,2000,3000]
 # parse
 input = readFile(fileName)
 bareNumbers = [int(i) for i in input.split('\n')]
-numbers = np.array([{'num':i, 'moved':False} for i in bareNumbers])
-print(json.dumps(list(numbers[0:3]),indent=4))
+numbers = [{'num':i, 'moved':False} for i in bareNumbers]
+print(json.dumps(numbers[0:3],indent=4))
 print([i['num'] for i in numbers[0:3]])
 print(min(bareNumbers))
 print(max(bareNumbers))
 
-for n in range(0,len(numbers)):
-    0
+totalAmount = len(numbers)
+for n in range(0,totalAmount):
+    curIndex = getNextNotMoved(numbers)
+    number = numbers.pop(curIndex)
+    number['moved'] = True
+    numbers.insert(getNewIndex(curIndex,number['num'],totalAmount), number)
+
+zeroIndex = [i for (i,n) in enumerate(numbers) if n['num']==0][0]
+getIndices = np.mod(np.add(after,zeroIndex), totalAmount)
+values =[j['num'] for j in [numbers[i] for i in getIndices]]
+print(' 0 on index: ' + str(zeroIndex))
+print(' get indices: '+str(getIndices))
+print(' values: '+ str(values))
+print(' sum:'+str(sum(values)))
+    
+    
