70bf92a48caa178ce78cef07b57326c1d529dcfe
bvanderhorn <bvanderhorn@gmail.com>
2022-12-27 19:00:15 +0100

    feat: day24 init

---
+++ 24/day24.py
@@ -0,0 +1,19 @@
+import re
+
+# functions
+def readFile(fName):
+    inStream = open(fName,'r')
+    out = inStream.read()
+    inStream.close()
+    return out
+
+def writeFile(fName,outString):
+    outStream = open(fName,'w')
+    outStream.write(outString)
+    outStream.close()
+    
+# params
+fileName = 'blizzards.txt'
+
+# parse
+input = readFile(fileName).split('\n')
