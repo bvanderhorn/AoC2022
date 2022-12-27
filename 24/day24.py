5c42d39da8e6fc1161626b1e86bcc8b8e8b6e9c4
bvanderhorn <bvanderhorn@gmail.com>
2022-12-27 23:03:56 +0100

    feat: day24 works with example

--- 24/day24.py
+++ 24/day24.py
@@ -1,5 +1,7 @@
 import re
 import numpy as np
+import timeit
+import time
 
 # functions
 def readFile(fName):
@@ -15,6 +17,9 @@ def writeFile(fName,outString):
     
 def mergeSubLists(list1):
     return [j for i in list1 for j in i]
+
+def moveLeft(inList, amount):
+    return inList[amount:] + inList[:amount]
     
 def getPositions(map,char):
     # for a given map and character (like '<'), return a list of [Y,X] positions
@@ -40,24 +45,51 @@ def moveAllMinutes(allPos, mins):
     
 def getNeighbours(pos):
     # return all possible neighbours (up/down/left/right) of current position
-    nb = [ [pos[0]-1, pos[1]], [pos[0]+1, pos[1]], [pos[0], pos[1]-1], [pos[0], pos[1]+1] ]
+    nb = [ [pos[0]-1, pos[1]], [pos[0]+1, pos[1]], [pos[0], pos[1]-1], [pos[0], pos[1]+1] , pos]
     return [i for i in nb if (i[0]>= 0) and (i[0] < Ylen) and (i[1]>= 0) and (i[1] < Xlen) ]
 
 def theoMin(pos,min):
     # calculate theoretical minimum minutes of reaching the goalPos from current position
     return sum(np.subtract(goalPos,pos)) + min
 
-def getMaxWithRemaining(bliz, pos, min, depth=0):
+def getFreeMinutes(pos, verbose = False):
+    # for a given [Y,X] position, return list of cycle minutes (1-600) in which it is free
+    if verbose: print(' free minutes calculation for '+ str(pos))
+    u0 = [u for u in uppies if u[1] == pos[1]]
+    d0 = [d for d in downies if d[1] == pos[1]]
+    l0 = [l for l in lefties if l[0] == pos[0]]
+    r0 = [r for r in righties if r[0] == pos[0]]
+    if verbose:
+        print('  blizzards on minute 0:')
+        print('  up: ' + str(u0))
+        print('  dn: ' + str(d0))
+        print('  le: ' + str(l0))
+        print('  ri: ' + str(r0))
+    
+    freeMinutes = []
+    for m in range(0,cycle):
+        bliz = moveAllMinutes([u0,d0,l0,r0],m)
+        if pos not in mergeSubLists(bliz):
+            freeMinutes.append(m)
+    
+    if verbose: print('  free minutes: '+ str(freeMinutes))
+    return freeMinutes
+
+def getMaxWithRemaining(pos, min, verbose=False, path=[], depth=0):
     # get minimum minutes of all possible remaining paths of reaching goalPos (or return INF if impossible)
-    # bliz in format [ups, downs, lefts, rights]
     global minMinutes
     global maxDepth
     d2 = depth*2
     d2a = d2+1
-    # print(' '*d2 + 'min '+str(min) + ', pos: '+str(pos))
+    if verbose: print(' '*d2 + 'min '+str(min) + ', pos: '+str(pos))
+    if len(path) >= cycle:
+        if path[len(path)- cycle] == pos:
+           if verbose: print(' '*d2 + '=> LOOPING: return INF') 
+           return inf
+    
     if depth > maxDepth:
         maxDepth = depth
-        print(' new max depth: '+str(depth))
+        # print(' new max depth: '+str(depth))
     
     if minMinutes <= theoMin(pos,min): return inf
     elif pos == goalPos:
@@ -65,38 +97,43 @@ def getMaxWithRemaining(bliz, pos, min, depth=0):
         print(' -> found new minimum: ' + str(min) + ' minutes')
         return min
     
-    # 1. move blizzards
-    newBliz = moveAllMinutes(bliz,1)
+    # 1. check which neighbours are free on the next minute
+    freeNb = []
+    for nb in  getNeighbours(pos):
+        if ((min+1) % cycle) in allFreeMinutes[nb[0]][nb[1]]:
+            freeNb.append(nb)
+    if verbose: print(' '*d2a + 'free nb: '+ str(freeNb))
     
-    # 2. check which neighbours are free
-    freeNb = [i for i in getNeighbours(pos) if i not in mergeSubLists(newBliz)]
-    # print(' '*d2a + 'free nb: '+ str(freeNb))
     if len(freeNb) == 0:
-        # print(' '*d2a + ' -> no neighbours: INF')
+        if verbose: print(' '*d2a + ' -> no neighbours: INF')
         return inf
     
-    # 3. loop and find minima for each option
+    # 2. loop and find minima for each option
     minMins = []
     for nb in freeNb:
-        nbMins = getMaxWithRemaining(newBliz,nb,min+1,depth+1)
+        newPath = [i for i in path]
+        newPath.append(pos)
+        nbMins = getMaxWithRemaining(nb,min+1,verbose,newPath,depth+1)
         minMins.append(nbMins)
         
     # 4. return the minimum of the found minima
-    # print(' '*d2 + 'return : ' + str(minMins))
+    if verbose: 
+        print(' '*d2 + 'return : ' + str(minMins))
     return sorted(minMins,reverse=True)[0]
 
-
 # params
-fileName = 'blizzards.txt'
-Xlen = 120
-Ylen = 25
+fileName = 'example_blizzards.txt'
 inf = 100000000000
 startPos = [0,0]
-goalPos = [24,119]
+# cycle = 600
+cycle = 12
 
 # parse
 input = readFile(fileName).split('\n')
 initialMap = [i[1:-1] for i in input[1:-1]]
+Xlen = len(initialMap[0])
+Ylen = len(initialMap)
+goalPos = [Ylen-1,Xlen-1]
 # print('\n'.join(initialMap))
 
 # extract positions of up/down/left/right going blizzards
@@ -106,25 +143,23 @@ lefties = getPositions(initialMap, '<')
 righties = getPositions(initialMap,'>')
 # print(lefties[0:10])
 
-# find possible start minutes: minutes (starting at 1) at which [0,0] is NOT occupied by a blizzard
-# possible start minutes are only influenced by ups/downs at X=0 and lefts/rights at Y=0
-# len/height = 120x25 -> blizzard pos repeats every 600 minutes
-u0 = [u for u in uppies if u[1] == 0]
-d0 = [d for d in downies if d[1] == 0]
-l0 = [l for l in lefties if l[0] == 0]
-r0 = [r for r in righties if r[0] == 0]
-
-# print(moveMinutes([1,2],'l',3))
-freeMinutes = []
-for m in range(1,601):
-    bliz0s = moveAllMinutes([u0,d0,l0,r0],m)
-    if [0,0] not in mergeSubLists(bliz0s):
-        freeMinutes.append(m)
-print(len(freeMinutes))
+allFreeMinutes = []
+for y in range(0,Ylen):
+    print(' calculating fm for y = '+ str(y)+'...')
+    start = timeit.default_timer()
+    allFreeMinutes.append([getFreeMinutes([y,x]) for x in range(0,Xlen)])
+    stop = timeit.default_timer()
+    runTime = time.strftime('%H:%M:%S', time.gmtime(stop - start))
+    print('  -> ' + runTime)
 
 # loop over all free minutes and try to find the shortest path
+fm0 = allFreeMinutes[0][0]
+if fm0[0] == 0:
+    fm0 = moveLeft(fm0,1)
+print(' free start minutes: '+ str(fm0))
 minMinutes = inf
-for fm in freeMinutes[0:1]:
+for fm in fm0:
+    maxDepth = 0
     print('start minute: ' + str(fm))
     if minMinutes <= theoMin(startPos, fm):
         print(' theoretically not possible to find a quicker solution with this start minute or higher -> BREAK')
@@ -132,7 +167,11 @@ for fm in freeMinutes[0:1]:
     # calculate start positions
     bliz = moveAllMinutes([uppies, downies, lefties, righties],fm)
     pos = startPos
-    maxDepth = 0
-    curMin = getMaxWithRemaining(bliz,pos,fm)
+    
+    curMin = getMaxWithRemaining(pos,fm)
     if curMin < minMinutes:
         minMinutes = curMin
+        
+print('\n'+'-'*30+'\n  minimal minutes: '+ str(minMinutes)+'\n'+'-'*30)
+
+getFreeMinutes([1,2],True)
